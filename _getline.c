6015546aac1da2664da1b4cf123ab0b484202a3b
gjdame <gregoryjdame@gmail.com>
2018-03-20 13:37:58 -0700

    getline update

--- _getline.c
+++ _getline.c
@@ -1,4 +1,11 @@
 #include "shell.h"
+int _fgetc (FILE *stream)
+{
+	int c;
+	int fd = (stream->_fileno);
+	c = read(fd, &c, 1);
+	return (c);
+}
 char *_fgets(char *s, int size, FILE *stream)
 {
 	char *ptr;
@@ -8,18 +15,18 @@ char *_fgets(char *s, int size, FILE *stream)
 	x = 0;
 	while (x < size)
 	{
-		if ((y = fgetc(stream) == EOF))
+		if ((y = _fgetc(stream) == EOF)) //get 1 char
 		{
-			*ptr = '\0';
+			*ptr = '\0'; //if end of file make null byte @ location
 			break;
 		}
-		if (y == '\n')
+		if (y == '\n') //if newline send back to _getline to handle
 			break;
-		*ptr = y;
-		ptr++;
-		x++;
+		*ptr = y; //else *ptr set to char from fgetc
+		ptr++; //increase ptr
+		x++; //increase x
 	}
-	*ptr = 0;
+	*ptr = 0; //set final ptr to null if goes through entire loop
 	return (ptr);
 }
        
@@ -28,27 +35,28 @@ int _getline(char **lineptr, size_t *n, FILE *stream)
 
 	static char line[1024];
 	char *c;
-	unsigned int len;
+	int increase;
+	int len;
 	
 	if (lineptr == NULL || n == NULL)
 		return (1);
 
-	fgets(line, 1024, stream);
+	_fgets(line, 1024, stream);
 
-	c = _strchr(line, '\n');
+	c = _strchr(line, '\n'); //search for instance of newline
 	if (c)
-		*c = '\0';
+		*c = '\0'; //replace with null
 	len = _strlen(line);
 
-	if ((len + 1) < 1024)
+	if ((len + 1) >= 1024) //check for need to realloc
 	{
-		c = realloc(*lineptr, 1024);
+		c = _realloc(*lineptr, 0, len + 1);
 		if (c == NULL)
 			return(-1);
 		*lineptr = c;
 		*n = 1024;
 	}
-	_strcpy(*lineptr,line);//strcpy
+	_strcpy(*lineptr,line);//copy line into lineptr
 	return(len);
 }
 
