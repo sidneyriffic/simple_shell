78689b9018e922cdbead188cfa970568e39bd6e0
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-03-22 23:53:00 -0700

    Fix cleanup of alias

---
+++ exitcleanup.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+#include "alias.h"
+#include "shellvars.h"
+
+char ***getenviron();
+ShellVar **getspecial();
+ShellVar **getvars();
+AliasData **getalist();
+
+void exitcleanup(char **av)
+{
+	ShellVar *sptr = *(getspecial()), *snext;
+	AliasData *aptr = *(getalist()), *anext;
+	char **environ = *(getenviron());
+	int i = 0;
+
+	if (av != NULL)
+		for (i = 0; av[i] != NULL; i++)
+			free(av[i]);
+	i = 0;
+	while (environ[i] != NULL)
+		free(environ[i++]);
+	free(environ);
+	while (sptr != NULL)
+	{
+		free(sptr->val);
+		free(sptr->name);
+		sptr = sptr->next;
+	}
+	free(*(getspecial()));
+	sptr = *(getvars());
+	while (sptr != NULL)
+	{
+		free(sptr->val);
+		free(sptr->name);
+		snext = sptr->next;
+		free(sptr);
+		sptr = snext;
+	}
+	while (aptr != NULL)
+	{
+		free(aptr->val);
+		free(aptr->name);
+		anext = aptr->next;
+		free(aptr);
+		aptr = anext;
+	}
+}
