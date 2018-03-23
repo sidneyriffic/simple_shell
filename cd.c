d2ea5c8575d6d0173549da7887f596a0ac1dee7e
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-03-23 07:39:53 -0700

    Fix cd error message for invalid directory

--- cd.c
+++ cd.c
@@ -33,55 +33,55 @@ int _cd(char *av[])
 	}
 	/*{*/
 #ifdef DEBUGCD
-		printf("Making new path %s:%c\n", av[1], av[1][0]);
+	printf("Making new path %s:%c\n", av[1], av[1][0]);
 #endif
-		newpath = malloc(sizeof(char) * (_strlen(oldpwd) + _strlen(av[1]) + 2));
-		if (newpath == NULL)
-			return (-1);
-		newptr = newpath;
-		pathbit = oldpwd;
-		if (av[1][0] != '/' && pathbit[1] != 0)
+	newpath = malloc(sizeof(char) * (_strlen(oldpwd) + _strlen(av[1]) + 2));
+	if (newpath == NULL)
+		return (-1);
+	newptr = newpath;
+	pathbit = oldpwd;
+	if (av[1][0] != '/' && pathbit[1] != 0)
+		while (*pathbit)
+			*newptr++ = *pathbit++;
+	*newptr++ = '/';
+	pathbit = strtok(av[1], "/");
+#ifdef DEBUGCD
+	printf("starting newpath:%s:Pathbit got:%s\n", newpath, pathbit);
+	printf("newpath/ptr diff:%p\n", newptr - newpath);
+#endif
+	while (pathbit != NULL)
+	{
+		if (pathbit[0] == '.' && pathbit[1] == '.'
+		    && pathbit[2] == 0)
+		{
+#ifdef DEBUGCD
+			printf("going back a directory%s:%s\n", newpath, newpath);
+#endif
+			newptr--;
+			if (newptr != newpath)
+				newptr--;
+			while (*newptr != '/')
+				newptr--;
+			*newptr++ = '/';
+		}
+		else if (!(pathbit[0] == '.' && pathbit[1] == 0))
+		{
 			while (*pathbit)
 				*newptr++ = *pathbit++;
-		*newptr++ = '/';
-		pathbit = strtok(av[1], "/");
-#ifdef DEBUGCD
-		printf("starting newpath:%s:Pathbit got:%s\n", newpath, pathbit);
-		printf("newpath/ptr diff:%p\n", newptr - newpath);
-#endif
-		while (pathbit != NULL)
-		{
-			if (pathbit[0] == '.' && pathbit[1] == '.'
-			    && pathbit[2] == 0)
-			{
-#ifdef DEBUGCD
-				printf("going back a directory%s:%s\n", newpath, newpath);
-#endif
-				newptr--;
-				if (newptr != newpath)
-					newptr--;
-				while (*newptr != '/')
-					newptr--;
-				*newptr++ = '/';
-			}
-			else if (!(pathbit[0] == '.' && pathbit[1] == 0))
-			{
-				while (*pathbit)
-					*newptr++ = *pathbit++;
-				*newptr++ = '/';
-			}
-			pathbit = strtok(NULL, "/");
-#ifdef DEBUGCD
-			printf("Got pathbit:%s\n", pathbit);
-#endif
+			*newptr++ = '/';
 		}
-		if (newptr != newpath && newptr != newpath + 1)
-			newptr--;
-		*newptr = 0;
+		pathbit = strtok(NULL, "/");
 #ifdef DEBUGCD
-		printf("New path:%s\n", newpath);
+		printf("Got pathbit:%s\n", pathbit);
 #endif
-		/*}*/
+	}
+	if (newptr != newpath && newptr != newpath + 1)
+		newptr--;
+	*newptr = 0;
+#ifdef DEBUGCD
+	printf("New path:%s\n", newpath);
+#endif
+	/*}*/
 	ret = chdir(newpath);
 	if (ret == 0)
 	{
@@ -93,7 +93,8 @@ int _cd(char *av[])
 		free(newpath);
 		return (0);
 	}
-	printerr(NULL);
+	printerr(": cd: can't cd to ");
+	fprintstrs(STDERR_FILENO, av[1], "\n", NULL);
 	free(oldpwd);
 	free(newpath);
 	return (ret);
