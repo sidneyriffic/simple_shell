598db1bf09a7efd678091bc5c400068ca54a48c0
gjdame <gregoryjdame@gmail.com>
2018-03-28 10:45:37 -0700

    history rework

---
+++ history.h
@@ -0,0 +1,16 @@
+#ifndef HISTORY_H
+#define HISTORY_H
+/**
+ * struct listhist - singly linked list
+ * @cmd: command
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct HistList
+{
+	char *cmd;
+	struct HistList *next;
+} HistList;
+#endif
